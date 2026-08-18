4d5d5814149d0ba92475911fa3d07d9ccfd585e5
kevin <ahadjon1999bb@gmail.com>
2026-08-18 16:09:21 +0900

    initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using System.Net;
+using System.Numerics;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+const string endPoint = "/kmc_08311_gmail_com";
+app.MapGet(endPoint, (string? x, string? y) =>
+{
+    if ( !BigInteger.TryParse(x, out BigInteger bigX) || bigX <= 0 ||
+        !BigInteger.TryParse(y, out BigInteger bigY) || bigY <= 0)
+    {
+        return Results.Text("NaN", "text/plain");
+    }  
+    BigInteger gcd = BigInteger.GreatestCommonDivisor(bigX, bigY);
+    Console.WriteLine(gcd);
+    BigInteger lcm = (bigX * bigY) / gcd;
+    return Results.Text(lcm.ToString(), "text/plain");
+});
+
+app.Run();
